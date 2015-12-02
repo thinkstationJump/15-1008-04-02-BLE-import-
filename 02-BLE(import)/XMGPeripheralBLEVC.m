7438a3d2bc6e4fe57d1982c75ea947cbcb8c6563
TimZInGitHub <Tim@11234.local>
2015-12-02 16:26:27 +0800

    15-1008-04-02-BLE(import)
    15-1008-04-02-BLE(import)

---
+++ 02-BLE(import)/XMGPeripheralBLEVC.m
@@ -0,0 +1,13 @@
+//
+//  XMGPeripheralBLEVC.m
+//  02-BLE(import)
+//
+//  Created by xiaomage on 15/10/8.
+//  Copyright © 2015年 小码哥. All rights reserved.
+//
+
+#import "XMGPeripheralBLEVC.h"
+
+@implementation XMGPeripheralBLEVC
+
+@end
